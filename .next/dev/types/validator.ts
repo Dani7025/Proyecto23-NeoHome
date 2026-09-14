95c517f9ffc678eda5bdf0bbd70c2b13ed101bd8
Dani7025 <angiedaniela7025@gmail.com>
2026-09-13 20:34:59 -0400

    Renombrar globals.txt a globals.css y page.txt a page.tsx

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -72,6 +72,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../../app/residente/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/residente">> = Specific
+  const handler = {} as typeof import("../../../app/residente/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
